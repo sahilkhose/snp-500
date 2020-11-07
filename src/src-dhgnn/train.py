bf60d6ae6d9db674a0c376bb217969fc3fba7cfa
Sahil Khose <sahilkhose18@gmail.com>
2020-11-07 09:30:12 +0530

    Run complete train

--- src/src-dhgnn/train.py
+++ src/src-dhgnn/train.py
@@ -73,36 +73,36 @@ def run():
 
     for epoch in range(1, config.args.EPOCHS+1): #TODO change this to while loop for inf train
         
-    #     #* Train metrics:
-    #     print("__"*80)
-    #     print("Epoch: ", epoch)
-    #     outputs_tr, targets_tr, loss_tr = engine.train_fn(train_data_loader, model, optimizer, device, epoch)
-    #     print(f"\nEpoch {epoch} Train metrics:")
-    #     acc, cm, mcc, f1 = engine.metrics_fn(outputs_tr, targets_tr, loss_tr)
+        #* Train metrics:
+        print("__"*80)
+        print("Epoch: ", epoch)
+        outputs_tr, targets_tr, loss_tr = engine.train_fn(train_data_loader, model, optimizer, device, epoch)
+        print(f"\nEpoch {epoch} Train metrics:")
+        acc, cm, mcc, f1 = engine.metrics_fn(outputs_tr, targets_tr, loss_tr)
 
-    #    #* All ones metrics:
-    #     if epoch == 1:
-    #         print("\nALL ONES")
-    #         outputs_t, targets_t, loss_t = engine.eval_fn(test_data_loader, model, device, epoch, "Test")
-    #         # engine.metrics_fn(list(np.ones((len(targets_t)))), targets_t, 0, all_ones=True)
-    #         all_ones_acc = metrics.accuracy_score(targets_t, list(np.ones((len(targets_t)))))
-    #         all_ones_cm = metrics.confusion_matrix(targets_t, list(np.ones((len(targets_t)))))
-    #         print(f"Accuracy  : {round(all_ones_acc, 4)}")
-    #         print(f"Confusion Matrix: \n{all_ones_cm}\n")
+       #* All ones metrics:
+        if epoch == 1:
+            print("\nALL ONES")
+            outputs_t, targets_t, loss_t = engine.eval_fn(test_data_loader, model, device, epoch, "Test")
+            # engine.metrics_fn(list(np.ones((len(targets_t)))), targets_t, 0, all_ones=True)
+            all_ones_acc = metrics.accuracy_score(targets_t, list(np.ones((len(targets_t)))))
+            all_ones_cm = metrics.confusion_matrix(targets_t, list(np.ones((len(targets_t)))))
+            print(f"Accuracy  : {round(all_ones_acc, 4)}")
+            print(f"Confusion Matrix: \n{all_ones_cm}\n")
 
-    #     #* Saving the metrics to a csv file (train_data_sheet.csv)
-    #     print(f"Saving metrics to: {config.TRAIN_DATA_SHEET}")
-    #     df = pd.read_csv(config.TRAIN_DATA_SHEET, index_col=[0])
-    #     df = df.append({
-    #     'model' : f"{config.args.NUM} epoch: {epoch}", 
-    #     'all_ones' : round(all_ones_acc, 4), 
-    #     'acc' : round(acc, 4), 
-    #     'mcc' : round(mcc, 4),
-    #     'f1' : round(f1, 4), 
-    #     'lr' : config.args.LR},  
-    #     ignore_index = True
-    #     ) 
-    #     df.to_csv(config.TRAIN_DATA_SHEET)
+        #* Saving the metrics to a csv file (train_data_sheet.csv)
+        print(f"Saving metrics to: {config.TRAIN_DATA_SHEET}")
+        df = pd.read_csv(config.TRAIN_DATA_SHEET, index_col=[0])
+        df = df.append({
+        'model' : f"{config.args.NUM} epoch: {epoch}", 
+        'all_ones' : round(all_ones_acc, 4), 
+        'acc' : round(acc, 4), 
+        'mcc' : round(mcc, 4),
+        'f1' : round(f1, 4), 
+        'lr' : config.args.LR},  
+        ignore_index = True
+        ) 
+        df.to_csv(config.TRAIN_DATA_SHEET)
        
         #* Validation, Testing and saving models:
         # if (epoch % config.args.EVAL_EVERY == 0) or (epoch % 50 == 0) or (epoch == config.args.EPOCHS):
@@ -116,16 +116,16 @@ def run():
         print(f"\nEpoch {epoch} Test metrics:")
         accuracy_t, cm_t, mcc_t, f1_t = engine.metrics_fn(outputs_t, targets_t, loss_t)
         
-        # #* Saving models and Confusion Matrix:
-        # if accuracy_t > best_accuracy:
-        #     engine.save_model("best", accuracy_t, all_ones_acc, model, epoch, cm_t, mcc_t, f1_t)
-        #     best_accuracy = accuracy_t
-        # elif (epoch % 5 == 0) or (epoch % 50 == 0):
-        #     engine.save_model("intermediate", accuracy_t, all_ones_acc, model, epoch, cm_t, mcc_t, f1_t)
-        # elif epoch == config.args.EPOCHS:
-        #     engine.save_model("last", accuracy_t, all_ones_acc, model, epoch, cm_t, mcc_t, f1_t)
-        # if epoch == 5:
-        #     break
+        #* Saving models and Confusion Matrix:
+        if accuracy_t > best_accuracy:
+            engine.save_model("best", accuracy_t, all_ones_acc, model, epoch, cm_t, mcc_t, f1_t)
+            best_accuracy = accuracy_t
+        elif (epoch % 5 == 0) or (epoch % 50 == 0):
+            engine.save_model("intermediate", accuracy_t, all_ones_acc, model, epoch, cm_t, mcc_t, f1_t)
+        elif epoch == config.args.EPOCHS:
+            engine.save_model("last", accuracy_t, all_ones_acc, model, epoch, cm_t, mcc_t, f1_t)
+        if epoch == 5:
+            break
 
 if __name__ == "__main__":
     run()
